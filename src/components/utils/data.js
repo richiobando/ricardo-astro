0084cb85c8967300f1f52c44c4a4bd3600241c0b
Ricardo <ricardobvil116@gmail.com>
2023-01-16 17:18:31 -0600

    refactor: :recycle: make cards scalable
    add file data for the proyects

---
+++ src/components/utils/data.js
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    repo: '',
+    title: 'Recipe Web App',
+    body: 'Serach the healtys recipe you want ',
+    image: '/food-app.png',
+    tools: ['ReactJS', 'CSS', 'NodeJs', 'PostgreSQL'],
+    deploy: '',
+  },
+  {
+    repo: '',
+    title: 'Gym Managment Web app',
+    body: 'Web app for any Gym ',
+    image: '/liberfit.png',
+    tools: ['ReactJS', 'CSS', 'NodeJs', 'PostgreSQL'],
+    deploy: '',
+  },
+];
