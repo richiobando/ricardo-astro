0a8f6d263da4bfe383690d7342ccfa077b79ecb1
Ricardo <ricardobvil116@gmail.com>
2023-01-16 23:11:28 -0600

    Style: :lipstick Change styles

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -1,18 +1,18 @@
 export const data = [
   {
-    repo: '',
+    repo: 'https://github.com/richiobando/food-app',
     title: 'Recipe Web App',
     body: 'Serach the healtys recipe you want ',
     image: '/food-app.png',
     tools: ['ReactJS', 'CSS', 'NodeJs', 'PostgreSQL'],
-    deploy: '',
+    deploy: 'food-app-client-p4p5.vercel.app/',
   },
   {
-    repo: '',
+    repo: 'https://github.com/richiobando/LiberFit-PF',
     title: 'Gym Managment Web app',
     body: 'Web app for any Gym ',
     image: '/liberfit.png',
-    tools: ['ReactJS', 'CSS', 'NodeJs', 'PostgreSQL'],
-    deploy: '',
+    tools: ['ReactJS', 'Typescript','Tailwind', 'NodeJs', 'PostgreSQL',],
+    deploy: 'liber-fit-pf-phi.vercel.app',
   },
 ];
