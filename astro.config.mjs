b29996412e830fb4d1f04da3633b93be5aede43e
Ricardo <ricardobvil116@gmail.com>
2023-01-18 00:02:48 -0600

    style: :lipstick: change styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
-
+import vercel from '@astrojs/vercel/serverless'
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
 	integrations: [mdx(), sitemap(),react()],
+	output: 'server',
+	adapter: vercel()
 });
