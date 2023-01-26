cc70bceed61294842a3ce533acc73a0f0a2dab4b
Ricardo <ricardobvil116@gmail.com>
2023-01-26 12:51:29 -0600

    fix: :bento: update assets links

--- src/components/utils/data.js
+++ src/components/utils/data.js
@@ -5,14 +5,14 @@ export const data = [
     body: 'Serach the healtys recipe you want ',
     image: '/food-app.png',
     tools: [{name:'ReactJS',icon:''}, {name:'CSS',icon:''}, {name:'NodeJs',icon:''}, {name:'PostgreSQL',icon:''}],
-    deploy: 'food-app-client-p4p5.vercel.app/',
+    deploy: 'https://food-app-client-p4p5.vercel.app/',
   },
   {
     repo: 'https://github.com/richiobando/LiberFit-PF',
     title: 'Gym Managment Web app',
     body: 'Web app for any Gym ',
-    image: '/liberfit.png',
+    image: '/Liberfit.png',
     tools: [{name:'ReactJS',icon:'#61dafb'}, {name:'Typescript',icon:'#007acc'},{name:'Tailwind',icon:''}, {name:'NodeJs',icon:'#83cd29'}, {name:'PostgreSQL',icon:'#336791'}],
-    deploy: 'liber-fit-pf-phi.vercel.app',
+    deploy: 'https://liber-fit-pf-phi.vercel.app/',
   },
 ];
